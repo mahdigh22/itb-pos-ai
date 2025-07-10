88bddb2d63358dcf40de0ae904837e7327f48bd0
mahdi ghoussein <mohamadmahdi.ghoussein@gmail.com>
2025-07-10 09:22:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect, useMemo } from 'react';
-import Link from 'next/link';
+import Link from 'next-intl/link';
 import { useRouter } from 'next/navigation';
 import { getCategories, getMenuItems } from '@/app/admin/menu/actions';
 import { getExtras } from '@/app/admin/extras/actions';
@@ -561,11 +561,11 @@ export default function Home() {
               <OrderSummary
                 activeCheck={activeCheck}
                 checks={checks}
-                onUpdateQuantity={handleUpdateQuantity}
-                onRemoveItem={handleRemoveItem}
-                onNewCheck={handleNewCheck}
-                onSendToKitchen={handleSendToKitchen}
-                onCloseCheck={handleCloseCheck}
+                onUpdateQuantity={onUpdateQuantity}
+                onRemoveItem={onRemoveItem}
+                onNewCheck={onNewCheck}
+                onSendToKitchen={onSendToKitchen}
+                onCloseCheck={onCloseCheck}
                 onClearCheck={handleClearCheck}
                 onCustomizeItem={handleStartCustomization}
                 onSwitchCheck={handleSwitchCheck}
