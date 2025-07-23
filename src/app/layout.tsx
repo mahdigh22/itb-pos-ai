90ad812887ad35c071ddfff7082994a02818c050
mahdigh22 <mohamadmahdi.ghoussein@gmail.com>
2025-07-24 00:42:20 +0300

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-        <link rel="icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icon-192x192.svg" />
+        <link rel="icon" href="/icon-192x192.svg" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
